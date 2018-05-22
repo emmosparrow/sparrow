d5436fad14fa29a59b93dae6a7f740d43be7061c
emmo sparrow <imteyazalambscit@gmail.com>
2018-05-22 17:56:17 +0530

    change hi

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express');
+var path = require('path');
 var app = express();
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var  ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 // index route............................
 app.get('/', function (req, res, next) {
-    res.send("sparrow is work");
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(port, function(){
